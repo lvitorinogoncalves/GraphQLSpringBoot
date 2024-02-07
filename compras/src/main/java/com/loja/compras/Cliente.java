62cf78a80d8b3f89076a7963aa95daf59af462e0
lvitorinogoncalves <lvitorinogoncalves@gmail.com>
2024-02-06 21:13:47 -0300

    Inclusão de schema e querys no graphql e dependencias GraphiQL e Palyground

---
+++ compras/src/main/java/com/loja/compras/Cliente.java
@@ -0,0 +1,15 @@
+package com.loja.compras;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente {
+
+    private String nome;
+    private String email;
+
+}

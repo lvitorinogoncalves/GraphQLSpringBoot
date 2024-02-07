dc319c937a24bc8e14d22cc5b39675340aff8669
lvitorinogoncalves <lvitorinogoncalves@gmail.com>
2024-02-07 16:26:27 -0300

    Mutation - Salvar Cliente

--- compras/src/main/java/com/loja/compras/graphql/ClienteGraphQL.java
+++ compras/src/main/java/com/loja/compras/graphql/ClienteGraphQL.java
@@ -1,15 +1,17 @@
 package com.loja.compras.graphql;
 
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.loja.compras.Cliente;
 import com.loja.compras.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Component
-public class ClienteGraphQL implements GraphQLQueryResolver {
+public class ClienteGraphQL implements GraphQLQueryResolver, GraphQLMutationResolver {
 
     @Autowired
     private ClienteRepository clienteRepository;
@@ -20,5 +22,12 @@ public class ClienteGraphQL implements GraphQLQueryResolver {
 
     public List<Cliente> clientes() {
         return clienteRepository.findAll();
+    }
+
+    @Transactional
+    public Cliente saveCliente(Long id, String nome, String email) {
+        Cliente cliente = new Cliente(id, nome, email);
+
+        return clienteRepository.save(cliente);
     }
 }
